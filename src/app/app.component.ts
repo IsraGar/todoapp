e5fb9fc21960f31f6b7c418830b214a33a9375cb
Israel Garcia <isragarciahdez@gmail.com>
2023-11-10 12:59:47 -0600

    Creacion del proyecto de Angular y primer ejecucion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,10 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todoapp';
+  welcome = 'Hola desde todoapp';
+  tasks = [
+    'Instalar Angular CLI',
+    'Crear proyecto',
+    'Crear componentes'
+  ]
 }
