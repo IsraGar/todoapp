04a7efb71bf78097150d2fdcbb7609815ba44c76
Israel Garcia <isragarciahdez@gmail.com>
2023-11-10 17:09:07 -0600

    Primeros pasos con property binding

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -14,5 +14,14 @@ export class LabsComponent {
     'Instalar Angular CLI',
     'Crear proyecto',
     'Crear componentes'
-  ]
+  ];
+  name = 'Isra';
+  age = 27;
+  disabled = true;
+  img = 'https://images.pexels.com/photos/18900657/pexels-photo-18900657/free-photo-of-luz-de-la-manana-en-la-puerta-de-brandenburgo.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load';
+  person = {
+    name: 'Isra',
+    age: 27,
+    img: 'https://images.pexels.com/photos/18900657/pexels-photo-18900657/free-photo-of-luz-de-la-manana-en-la-puerta-de-brandenburgo.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+  };
 }
