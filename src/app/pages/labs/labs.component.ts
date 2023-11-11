a1828a61d6bc0bd1da412fb103458ad79d9602c5
Israel Garcia <isragarciahdez@gmail.com>
2023-11-11 06:57:02 -0600

    Creando un signal de Angular

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { signal } from "@angular/core";
 
 @Component({
   selector: 'app-labs',
@@ -15,7 +16,7 @@ export class LabsComponent {
     'Crear proyecto',
     'Crear componentes'
   ];
-  name = 'Isra';
+  name = signal('Isra');
   age = 27;
   disabled = true;
   img = 'https://images.pexels.com/photos/18900657/pexels-photo-18900657/free-photo-of-luz-de-la-manana-en-la-puerta-de-brandenburgo.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load';
@@ -30,6 +31,13 @@ export class LabsComponent {
   }
 
   changeHandler(event: Event){
-    console.log(event);    
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.name.set(newValue);
+  }
+
+  keydownHandler(event: KeyboardEvent){
+    const input = event.target as HTMLInputElement;
+    console.log(input.value);
   }
 }
