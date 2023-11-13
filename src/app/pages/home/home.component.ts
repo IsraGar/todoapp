1182d8256c7fbff559b8e051cc30ec3c15d1fccb
Israel Garcia <isragarciahdez@gmail.com>
2023-11-13 11:28:15 -0600

    Implmentando agregar y eliminar tarea

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,10 +9,24 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
   tasks = signal([
     'Instalar Angular CLI',
     'Crear proyecto',
     'Crear componentes',
     'Crear servicio'
   ]);
+
+  changeHandler(event: Event){
+    const input = event.target as HTMLInputElement;
+    const newTask = input.value;
+    this.tasks.update((tasks)=> [...tasks, newTask]);
+  }
+
+  deleteTask(index: number){
+    this.tasks.update((tasks) => {
+      tasks.splice(index, 1);
+      return tasks;
+    });
+  }
 }
