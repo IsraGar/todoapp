e5fb9a0cf9522a575cb8ebfe499bbcb79bb800cf
Israel Garcia <isragarciahdez@gmail.com>
2023-11-14 16:43:46 -0600

    ngIf y else con ng-template

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -20,11 +20,11 @@ export class LabsComponent {
   age = 27;
   disabled = true;
   img = 'https://images.pexels.com/photos/18900657/pexels-photo-18900657/free-photo-of-luz-de-la-manana-en-la-puerta-de-brandenburgo.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load';
-  person = {
+  person = signal({
     name: 'Isra',
     age: 27,
     img: 'https://images.pexels.com/photos/18900657/pexels-photo-18900657/free-photo-of-luz-de-la-manana-en-la-puerta-de-brandenburgo.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
-  };
+  });
 
   clickHandler(){
     alert('Hola');
@@ -39,5 +39,16 @@ export class LabsComponent {
   keydownHandler(event: KeyboardEvent){
     const input = event.target as HTMLInputElement;
     console.log(input.value);
+  }
+
+  changeAge(event: Event){
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.person.update(prevState => {
+      return{
+        ...prevState,
+        age: parseInt(newValue, 10)
+      }
+    })
   }
 }
